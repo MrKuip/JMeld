e947ec9164420b2958aedf216674e057f0d14b1f
Kees Kuip <keeskuip@cubeautomatisering.nl>
2025-11-04 12:32:41 +0100

    Fix Directory scanner test

--- src/test/java/org/jmeld/util/file/DirectoryScannerTest.java
+++ src/test/java/org/jmeld/util/file/DirectoryScannerTest.java
@@ -19,27 +19,27 @@ public class DirectoryScannerTest
     List<Path> fileList;
     List<Path> directoryList;
     Path userDir;
-    
+
     userDir = Paths.get(System.getProperty("user.dir"));
     scanner = new DirectoryScanner(userDir);
     fileList = scanner.scan();
     directoryList = scanner.getDirectoryList();
-    
+
     assertTrue(fileList.contains(userDir.resolve("src/main/java/org/jmeld/Main.java")));
     assertTrue(fileList.contains(userDir.resolve("src/test/java/org/jmeld/util/file/DirectoryScannerTest.java")));
     assertTrue(fileList.contains(userDir.resolve("src/main/resources/help/images/keyCTRL.png")));
-    
+
     assertTrue(directoryList.contains(userDir.resolve("src/test")));
     assertTrue(directoryList.contains(userDir.resolve("src/main/java")));
     assertTrue(directoryList.contains(userDir.resolve(".git")));
     assertTrue(fileList.contains(userDir.resolve(".git/HEAD")));
     assertTrue(directoryList.contains(userDir.resolve("build")));
-    
+
     assertTrue(fileList.stream().filter(file -> file.toString().endsWith(".png")).findFirst().isPresent());
     assertTrue(fileList.stream().filter(file -> file.toString().endsWith(".class")).findFirst().isPresent());
-    assertTrue(fileList.stream().filter(file -> file.toString().endsWith(".xml")).findFirst().isPresent());
+    assertTrue(fileList.stream().filter(file -> file.toString().endsWith(".htm")).findFirst().isPresent());
   }
-  
+
   @Test
   public void testScanDefault()
   {
@@ -48,31 +48,31 @@ public class DirectoryScannerTest
     List<Path> directoryList;
     Path userDir;
     Filter filter;
-    
+
     filter = JMeldSettings.getInstance().getFilter().getFilter(FilterSettings.DEFAULT);
-    
+
     userDir = Paths.get(System.getProperty("user.dir"));
     scanner = new DirectoryScanner(userDir);
     scanner.setIncludes(filter.getIncludes());
     scanner.setExcludes(filter.getExcludes());
     fileList = scanner.scan();
     directoryList = scanner.getDirectoryList();
-    
+
     assertTrue(fileList.contains(userDir.resolve("src/main/java/org/jmeld/Main.java")));
     assertTrue(fileList.contains(userDir.resolve("src/test/java/org/jmeld/util/file/DirectoryScannerTest.java")));
     assertFalse(fileList.contains(userDir.resolve("src/main/resources/help/images/keyCTRL.png")));
-    
+
     assertTrue(directoryList.contains(userDir.resolve("src/test")));
     assertTrue(directoryList.contains(userDir.resolve("src/main/java")));
     assertFalse(directoryList.contains(userDir.resolve(".git")));
     assertFalse(directoryList.contains(userDir.resolve(".git/HEAD")));
     assertTrue(directoryList.contains(userDir.resolve("build")));
-    
+
     assertFalse(fileList.stream().filter(file -> file.toString().endsWith(".png")).findFirst().isPresent());
     assertFalse(fileList.stream().filter(file -> file.toString().endsWith(".class")).findFirst().isPresent());
-    assertTrue(fileList.stream().filter(file -> file.toString().endsWith(".xml")).findFirst().isPresent());
+    assertTrue(fileList.stream().filter(file -> file.toString().endsWith(".htm")).findFirst().isPresent());
   }
-  
+
   @Test
   public void testScanExcludes()
   {
@@ -81,30 +81,30 @@ public class DirectoryScannerTest
     List<Path> directoryList;
     Path userDir;
     Filter filter;
-    
+
     filter = JMeldSettings.getInstance().getFilter().getFilter(FilterSettings.DEFAULT);
-    
+
     userDir = Paths.get(System.getProperty("user.dir"));
     scanner = new DirectoryScanner(userDir);
     scanner.setIncludes(filter.getIncludes());
     scanner.setExcludes(filter.getExcludes());
     scanner.addExclude("**/test");
-    scanner.addExclude("**/*.xml");
+    scanner.addExclude("**/*.htm");
     fileList = scanner.scan();
     directoryList = scanner.getDirectoryList();
-    
+
     assertFalse(directoryList.contains(userDir.resolve("src/test")));
     assertTrue(directoryList.contains(userDir.resolve("src/main/java")));
     assertTrue(fileList.contains(userDir.resolve("src/main/java/org/jmeld/Main.java")));
     assertFalse(fileList.contains(userDir.resolve("src/test/java/org/jmeld/util/file/DirectoryScannerTest.java")));
     assertFalse(fileList.contains(userDir.resolve("src/main/resources/help/images/keyCTRL.png")));
-    
+
     assertFalse(directoryList.contains(userDir.resolve(".git")));
     assertFalse(directoryList.contains(userDir.resolve(".git/HEAD")));
     assertTrue(directoryList.contains(userDir.resolve("build")));
-    
+
     assertFalse(fileList.stream().filter(file -> file.toString().endsWith(".png")).findFirst().isPresent());
     assertFalse(fileList.stream().filter(file -> file.toString().endsWith(".class")).findFirst().isPresent());
-    assertFalse(fileList.stream().filter(file -> file.toString().endsWith(".xml")).findFirst().isPresent());
+    assertFalse(fileList.stream().filter(file -> file.toString().endsWith(".htm")).findFirst().isPresent());
   }
 }
